d5ffea943f719f7d1e906c8b515991ce2fb96ad4
Yax <kianby@madyanne.fr>
2017-08-13 09:00:48 +0200

    Update timer example

--- examples/timer/test.py
+++ examples/timer/test.py
@@ -6,14 +6,19 @@ sys.path.append('../..')
 import time
 from datetime import datetime
 from microsvax.service import timer
-from microsvax.service import event
 from microsvax import mservice
 
 
 @timer(seconds=1)
 def every1():
     value = datetime.now()
-    print('date {}'.format(value))
+    print('date1 {}'.format(value))
+
+
+@timer(seconds=3)
+def every3():
+    value = datetime.now()
+    print('date3 {}'.format(value))
 
 
 time.sleep(10)
